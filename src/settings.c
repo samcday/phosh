samcday/phosh

commit af37b2c74278b59fe07163399cd33a5b7d3e62fb

settings: Add action to open mobile settings
We add a separate action as that makes it more obvious what we're
opening.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1705>

src/settings.c
@@ -222,12 +222,16 @@ brightness_value_changed_cb (GtkScale *scale_brightness, gpointer unused)
 
 
 static void
-open_settings_panel (PhoshSettings *self, const char *panel)
+open_settings_panel (PhoshSettings *self, gboolean mobile, const char *panel)
 {
   if (self->on_lockscreen)
     return;
 
-  phosh_util_open_settings_panel (panel);
+  if (mobile)
+    phosh_util_open_mobile_settings_panel (panel);
+  else
+    phosh_util_open_settings_panel (panel);
+
   close_settings_menu (self);
 }
 
@@ -240,7 +244,20 @@ on_launch_panel_activated (GSimpleAction *action, GVariant *param, gpointer data
 
   panel = g_variant_get_string (param, NULL);
 
-  open_settings_panel (self, panel);
+  open_settings_panel (self, FALSE, panel);
+  phosh_settings_hide_details (self);
+}
+
+
+static void
+on_launch_mobile_panel_activated (GSimpleAction *action, GVariant *param, gpointer data)
+{
+  PhoshSettings *self = PHOSH_SETTINGS (data);
+  const char *panel;
+
+  panel = g_variant_get_string (param, NULL);
+
+  open_settings_panel (self, TRUE, panel);
   phosh_settings_hide_details (self);
 }
 
@@ -536,6 +553,9 @@ phosh_settings_class_init (PhoshSettingsClass *klass)
 
 static const GActionEntry entries[] = {
   { .name = "launch-panel", .activate = on_launch_panel_activated, .parameter_type = "s" },
+  { .name = "launch-mobile-panel",
+    .activate = on_launch_mobile_panel_activated,
+    .parameter_type = "s" },
 };
 
 

commit ac8fd21aab3d1ea6d4928983f7eec6fbc15d00c5

settings: Disable launching mobile panels on lockscreen too
Fixes: af37b2c74 ("settings: Add action to open mobile settings")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1768>

src/settings.c
@@ -581,6 +581,11 @@ phosh_settings_init (PhoshSettings *self)
                           action, "enabled",
                           G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
 
+  action = g_action_map_lookup_action (map, "launch-mobile-panel");
+  g_object_bind_property (self, "on-lockscreen",
+                          action, "enabled",
+                          G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
+
   g_signal_connect (self, "size-allocate", G_CALLBACK (on_size_allocate), NULL);
 }
 