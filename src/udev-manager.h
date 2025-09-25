43ee450bc861442a79d6585935c423fcab6b82ae
Guido Günther <agx@sigxcpu.org>
2025-09-25 15:37:42 +0200

    udev-manager: New class to handle udev interaction
    We need a client for torch and backlight. Track it in a common class
    and add the helpers we'll need for backlight handling.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1774>

---
+++ src/udev-manager.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2025 Phosh.mobi e.V.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include "dbus/login1-session-dbus.h"
+
+#include <glib-object.h>
+
+#include <gudev/gudev.h>
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_UDEV_MANAGER (phosh_udev_manager_get_type ())
+
+G_DECLARE_FINAL_TYPE (PhoshUdevManager, phosh_udev_manager, PHOSH, UDEV_MANAGER, GObject)
+
+PhoshUdevManager *     phosh_udev_manager_get_default (void);
+GUdevDevice *          phosh_udev_manager_find_backlight (PhoshUdevManager *self,
+                                                          const char *connector_name);
+
+G_END_DECLS
