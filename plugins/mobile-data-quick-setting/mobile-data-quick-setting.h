c1392f0e2986bf9f298a539c71e356d3c9a70f27
Arun Mani J <j.arunmani@proton.me>
2025-10-22 12:00:48 +0530

    mobile-data-quick-setting: Simplify files
    Signed-off-by: Arun Mani J <j.arunmani@proton.me>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1799>

--- plugins/mobile-data-quick-setting/mobile-data-quick-setting.h
+++ plugins/mobile-data-quick-setting/mobile-data-quick-setting.h
@@ -6,15 +6,14 @@
  * Author: Guido Günther <agx@sigxcpu.org>
  */
 
-#include <gtk/gtk.h>
+#pragma once
 
 #include "quick-setting.h"
 
-#pragma once
-
 G_BEGIN_DECLS
 
-#define PHOSH_TYPE_MOBILE_DATA_QUICK_SETTING (phosh_mobile_data_quick_setting_get_type ())
+#define PHOSH_TYPE_MOBILE_DATA_QUICK_SETTING phosh_mobile_data_quick_setting_get_type ()
+
 G_DECLARE_FINAL_TYPE (PhoshMobileDataQuickSetting,
                       phosh_mobile_data_quick_setting,
                       PHOSH, MOBILE_DATA_QUICK_SETTING, PhoshQuickSetting)
